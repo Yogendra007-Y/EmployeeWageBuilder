69c6aba5451a1d535a7027b1ea1f6da13da92a9b
Yogendra <dharmayog91@gmail.com>
2021-09-16 20:25:42 +0530

    ADD PART TIME EMPLOYEE WAGE

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -6,24 +6,32 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
         int IS_FULL_TIME = 1;
+        int IS_FULL_TIME1 = 2;
+        int IS_PART_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
         int empHours = 0;
         int empWage = 0;
         // Taking Random values 0 or 1.
         Random random = new Random();
         int empCheck = random.nextInt(2);
+        int empCheck1 = random.nextInt(3);
         // Checking the condition.
         if (empCheck == IS_FULL_TIME){
             System.out.println("Employee is Present");
-            empHours = 8;
+            if (empCheck1 == IS_FULL_TIME1) {
+                empHours = 8;
+            }
+            else {
+                System.out.println("Employee is Absent");
+            } else if (empCheck1 == IS_PART_TIME) {
+                empHours = 4;
+            } else {
+                empHours = 0;
+            }
+            empWage = empHours * EMP_RATE_PER_HOUR;
+            System.out.println("The daily wages of an employee is : " + empWage);
+
         }
-        else {
-            System.out.println("Employee is Absent");
-            empHours = 0;
-        }
-        empWage = empHours * EMP_RATE_PER_HOUR;
-        System.out.println("The daily wages of an employee is : " + empWage);
     }
 }
