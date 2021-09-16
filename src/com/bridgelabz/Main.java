6c9ad75a91bcb4e641a2f12ced2773fd2d63d9c5
Yogendra <dharmayog91@gmail.com>
2021-09-16 20:21:35 +0530

    CALCULATE EMP DAILY WAGE

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -8,13 +8,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHours = 0;
+        int empWage = 0;
+        // Taking Random values 0 or 1.
         Random random = new Random();
         int empCheck = random.nextInt(2);
+        // Checking the condition.
         if (empCheck == IS_FULL_TIME){
             System.out.println("Employee is Present");
+            empHours = 8;
         }
         else {
             System.out.println("Employee is Absent");
+            empHours = 0;
         }
+        empWage = empHours * EMP_RATE_PER_HOUR;
+        System.out.println("The daily wages of an employee is : " + empWage);
     }
 }
