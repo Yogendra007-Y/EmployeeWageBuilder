fa6e06bb144d9b33f74508503c45899f818fa7b1
Yogendra <dharmayog91@gmail.com>
2021-09-16 22:02:26 +0530

    SOLVING USE CASE STATEMENTS

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -5,10 +5,15 @@ import java.util.Random;
 
 public class Main {
 
+    public static final int IS_FULL_TIME = 2;
+    public static final int IS_PART_TIME = 1;
+    public static final int EMP_RATE_PER_HOUR = 20;
+
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
-        int IS_FULL_TIME1 = 2;
-        int IS_PART_TIME = 1;
+        int IS_FULL_TIME1 = 1;
+        int IS_FULL_TIME2 = 2;
+        int IS_PART_TIME1 = 1;
+        int IS_PART_TIME2 = 1;
         int EMP_RATE_PER_HOUR = 20;
         int empHours = 0;
         int empWage = 0;
@@ -16,21 +21,29 @@ public class Main {
         Random random = new Random();
         int empCheck = random.nextInt(2);
         int empCheck1 = random.nextInt(3);
+        int empCheck2 = random.nextInt(3);
         // Checking the condition.
-        if (empCheck == IS_FULL_TIME){
-            System.out.println("Employee is Present");
-            if (empCheck1 == IS_FULL_TIME1) {
+        if (empCheck1 == IS_FULL_TIME1) {
+            if (empCheck == IS_FULL_TIME1) {
                 empHours = 8;
-            }
-            else {
-                System.out.println("Employee is Absent");
             } else if (empCheck1 == IS_PART_TIME) {
+            } else if (empCheck == IS_PART_TIME) {
                 empHours = 4;
             } else {
                 empHours = 0;
+                switch (empCheck) {
+                    case IS_FULL_TIME2:
+                        empHours = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHours = 4;
+                        break;
+                    default:
+                        empHours = 0;
+                }
+                empWage = empHours * EMP_RATE_PER_HOUR;
+                System.out.println("The daily wages of an employee is : " + empWage);
             }
-            empWage = empHours * EMP_RATE_PER_HOUR;
-            System.out.println("The daily wages of an employee is : " + empWage);
 
         }
     }
